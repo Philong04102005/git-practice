0628e46df913468628c632b7af38b6fd554992fd
Philong04102005 <philong04102005@gmail.com>
2025-11-03 12:09:48 +0700

    First commit

--- Facebook/data/content/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-curl = "https://docs.google.com/spreadsheets/d/1dAzxOmTbrrekYvk9HqIVBq7A1c61k4kaGOm42w3kGlM/edit?gid=1583500962#gid=1583500962"
-
-df = pd.read_csv(curl)
-print(df)
--- read_data.py
+++
@@ -1,4 +0,0 @@
-print("Read data")
-print("commit --amend")
-print("commit --amend 2")
-print("commit --amend 3")
